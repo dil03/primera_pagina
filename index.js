8d11a11f74ed55610d8ae3a7ac40c894f56baf84
Dilan <dilanalfaro2003@gamil.com>
2023-10-29 14:20:26 -0600

    primer comit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*function palindrome(palabra){
+    const palabrainvertida = palabra.split("").reverse().join("")
+    if (palabrainvertida == palabra){ 
+        return true
+    }else{
+        return false
+    }
+    
+}
+console.log(palindrome("121"))
+console.log(palindrome("-121"))
+console.log(palindrome("10"))*/
+
+
+
+
+
